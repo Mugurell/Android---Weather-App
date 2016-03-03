e8ac83123818bb3257d372caec1fdc83518523a1
Mugurell <petrumugurel@gmail.com>
2016-03-03 22:13:53 +0200

    Finished v1. Can be added a ton on it. Cool app

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-As a background used a photo by Ales Krivec - https://unsplash.com/photos/ywtbSuCSjhM
+Everybody has to have a weather app under it's belt, right?
+
+Here's mine.
+
+[![Android - weather app](http://oi66.tinypic.com/34eb32g.jpg)](https://www.youtube.com/watch?v=EhTMw2BPakU "Android - weather app")
+
+
+
+Simple app (can be built a lot on top of it, if time) which uses data from
+http://api.openweathermap.org/data/2.5  similar to  
+`{"coord":{"lon":2.35,"lat":48.85},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"02n"}],"base":"cmc stations","main":{"temp":277.571,"pressure":1009.4,"humidity":67,"temp_min":277.571,"temp_max":277.571,"sea_level":1021.74,"grnd_level":1009.4},"wind":{"speed":2.18,"deg":286.001},"clouds":{"all":8},"dt":1457032380,"sys":{"message":0.006,"country":"FR","sunrise":1456986378,"sunset":1457026741},"id":2988507,"name":"Paris","cod":200}`
+
+from which with simple JSON methods various weather properties are extracted.
+
+Nothing too complicated but I'm really proud of a method I've built which allows to easily query  the openweathermap forecast JSON to get any property(es) desired.
+```java
+/**
+ * Cool method which allows to query a JSONObject based on various fields - basically any
+ * name-value pair that is contained in the result from
+ * {@code http://api.openweathermap.org/data/2.5 ...}.
+ *
+ * @param forecast JSONObject representing the response get from interrogating
+ *                                  {@code http://api.openweathermap.org/data/2.5...}
+ *                                  for the weather in a specific city.<br><br>
+ * @param forecastQueriedProperties varying number of Strings representing name-value pairs
+ *                                  for which to query the {@code forecast} JSONObject
+ * @return The values for the queried fields, in the query order.
+ */
+        /** This will store the forecast to be returned */
+    ArrayList<String> weather = new ArrayList<>(forecastQueriedProperties.length);
+//        Log.d("Forecast:", forecast.toString());
+
+    try {
+        for (String[] properties : forecastQueriedProperties) {
+            JSONObject tempJSON = forecast;
+            String queriedProperty = "";
+            for (int i = 0; i < properties.length; i++) {
+//                    Log.d("Querying JSON for ", properties[i]);
+                if (i + 1 < properties.length) {
+//                        Log.e("Current JSON", tempJSON.getString(properties[i]));
+                    if (properties[i].equals("weather")) {  // weather is a JSONArray with 1 elm
+                        String wS = tempJSON.getString(properties[i]);
+                        tempJSON = new JSONArray(wS).getJSONObject(0);
+                    }
+                    else {
+                        tempJSON = new JSONObject(tempJSON.getString(properties[i]));
+                    }
+                }
+                else {  // we have the queried forecast property
+                    queriedProperty = tempJSON.getString(properties[i]);
+                }
+
+                if (properties[i].equals("temp")) {
+                    queriedProperty = getCelsiusFromKelvin(queriedProperty);
+                }
+            }
+            weather.add(queriedProperty);
+//                Log.d("Current weather is ", weather.toString());
+        }
+    } catch (JSONException e) {
+        e.printStackTrace();
+    }
+
+    return weather;
+}
+```
+
+
+As background image used a photo by Ales Krivec - https://unsplash.com/photos/ywtbSuCSjhM
 
--- app/src/main/java/site/petrumugurel/weatherapp/MainActivity.java
+++ app/src/main/java/site/petrumugurel/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package site.petrumugurel.weatherapp;
 
 import android.app.NotificationManager;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,8 +12,8 @@ import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -26,6 +27,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String OWM_API_KEY           = "&APPID=3c74a8bc2fd594a33ed13471ca2ad580";
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText mEditText;
     TextView mTextView;
-    Button   mButton;
+//    Button   mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,47 +58,160 @@ public class MainActivity extends AppCompatActivity {
 
         mEditText = (EditText) findViewById(R.id.mainA_RL_ET_enterCity);
         mTextView = (TextView) findViewById(R.id.mainA_RL_TV_forecast);
-        mButton = (Button) findViewById(R.id.mainA_RL_BTN_go);
+//        mButton = (Button) findViewById(R.id.mainA_RL_BTN_go);
 
+        // To allow starting the query when user presses "Enter" on the softkey keyboard.
         mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (event != null) {
+                if ((actionId == EditorInfo.IME_ACTION_DONE)
+                    || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER)
+                        && (event.getAction() == KeyEvent.ACTION_DOWN))) {
+
+                    // To hide the soft keyboard when user presses "Enter"
+                    InputMethodManager imm
+                            = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
                     String city = mEditText.getText().toString();
                     if (isValidCity(city)) {
-                        getForecastForCity(city);
+                        JSONObject forecast = downloadForecastForCity(city);
+                        ArrayList<String> weatherInfo
+                                = getWeatherInfo(forecast,
+                                                 new String[]{"name"},
+                                                 new String[]{"sys", "country"},
+                                                 new String[]{"weather", "description"},
+                                                 new String[]{"main", "temp"},
+                                                 new String[]{"main", "pressure"});
+
+                        StringBuilder sb = new StringBuilder(weatherInfo.get(2))
+                                .append(" in ").append(weatherInfo.get(0)).append(" - ")
+                                .append(weatherInfo.get(1)).append(" with a temperature of ")
+                                .append(String.format
+                                        ("%.1f", Float.parseFloat(weatherInfo.get(3))))
+                                .append(" °C");
+                        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                        mTextView.setText(sb.toString());
                     }
                 }
                 return true;
             }
         });
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String city = mEditText.getText().toString();
-                if (isValidCity(city)) {
-                    getForecastForCity(city);
-                }
-            }
-        });
-
     }
 
+
+    /**
+     * Cool method which allows to query a JSONObject based on various fields - basically any
+     * name-value pair that is contained in the result from
+     * {@code http://api.openweathermap.org/data/2.5 ...}.
+     *
+     * @param forecast JSONObject representing the response get from interrogating
+     *                                  {@code http://api.openweathermap.org/data/2.5...}
+     *                                  for the weather in a specific city.<br><br>
+     * @param forecastQueriedProperties varying number of Strings representing name-value pairs
+     *                                  for which to query the {@code forecast} JSONObject
+     * @return The values for the queried fields, in the query order.
+     */
+    private ArrayList<String> getWeatherInfo(JSONObject forecast, String[]...
+            forecastQueriedProperties) {
+
+        /** This will store the forecast to be returned */
+        ArrayList<String> weather = new ArrayList<>(forecastQueriedProperties.length);
+//        Log.e("Forecast:", forecast.toString());
+
+        try {
+            for (String[] properties : forecastQueriedProperties) {
+                JSONObject tempJSON = forecast;
+                String queriedProperty = "";
+                for (int i = 0; i < properties.length; i++) {
+//                    Log.e("Querying JSON for ", properties[i]);
+                    if (i + 1 < properties.length) {
+//                        Log.e("Current JSON", tempJSON.getString(properties[i]));
+                        // weather is a JSONArray with just one element
+                        if (properties[i].equals("weather")) {
+                            String wS = tempJSON.getString(properties[i]);
+                            tempJSON = new JSONArray(wS).getJSONObject(0);
+                        }
+                        else {
+                            tempJSON = new JSONObject(tempJSON.getString(properties[i]));
+                        }
+                    }
+                    else {  // we have the queried forecast property
+                        queriedProperty = tempJSON.getString(properties[i]);
+                    }
+
+                    if (properties[i].equals("temp")) {
+                        queriedProperty = getCelsiusFromKelvin(queriedProperty);
+                    }
+                }
+                weather.add(queriedProperty);
+//                Log.d("Current weather is ", weather.toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return weather;
+    }
+
+    private String getCelsiusFromKelvin(String kelvinTemp) {
+        double kelvins = Double.parseDouble(kelvinTemp.toString());
+        double celsius = kelvins - 273.15;
+
+        return (Double.toString(celsius));
+    }
+
+    /**
+     * Simple check to mitigate invalid queries for invalid cities. Can be made more complex later.
+     * @param city name of the city to get the weather for, as entered by the user.
+     * @return
+     */
     private boolean isValidCity(String city) {
         if (city.length() <= 2) {
             Snackbar.make(findViewById(android.R.id.content),
-                          "Too few characters for the city", Snackbar.LENGTH_LONG).show();
+                          "Too few characters for the city name", Snackbar.LENGTH_LONG).show();
             return false;
         }
         return true;
     }
 
-    private void getForecastForCity(String city) {
-        new DownloadCurrentWeather().execute(WEATHER_QUERY_URL + city + OWM_API_KEY);
+    /**
+     * Will use an AsyncTask to download the response from
+     * {@code http://api.openweathermap.org/data/2.5...}
+     * representing the forecast for a specific city.
+     * <br>It assumes that static fields {@link #WEATHER_QUERY_URL} and {@link #OWM_API_KEY}
+     * contain valid data for constructing the URL for the {@code openweathermap} query.
+     * @param city Name of the city for which the user wants to get current weather.
+     * @return The forecast returned by {@code http://api.openweathermap.org/data/2.5...} .
+     */
+    private JSONObject downloadForecastForCity(String city) {
+        // An ASCII space in the city will break the query on openweathermap
+        city = city.replaceAll(" ", "%20");
+        String forecast = "";
+        try {
+            forecast = new DownloadCurrentWeather()
+                    .execute(WEATHER_QUERY_URL + city + OWM_API_KEY).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            return new JSONObject(forecast);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
+    /**
+     * To be used for downloading the response from interrogating
+     * {@code http://api.openweathermap.org/data/2.5...}
+     * for the weather of a specific city.
+     */
     private class DownloadCurrentWeather extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
@@ -144,47 +260,22 @@ public class MainActivity extends AppCompatActivity {
 
             Log.i(MainActivity.class.getSimpleName(), result);
 
-            String weather = "";
-
-            // To extract the weather data
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                // extract the weather part of the JSON Object
-                String weatherInfo = jsonObject.getString("weather");
-                Log.i(MainActivity.class.getSimpleName(), weatherInfo);
-
-                // to loop through the weatherInfo mini JSON
-                JSONArray jsonArray = new JSONArray(weatherInfo);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    // the weatherInfo (a String) can be seen as an array with 2 elements - 2 jsons.
-                    JSONObject jsonPart = jsonArray.getJSONObject(i);
-                    System.out.println("Arr " + i + " : "+ jsonPart.toString());
-                    Log.i("MAIN: ", jsonPart.getString("main") + " - "
-                                    + jsonPart.getString("description"));
-
-                    weather = jsonPart.getString("main") + " - "
-                              + jsonPart.getString("description");
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            mTextView.setText(weather);
-
-            mEditText.animate().alpha(0).setDuration(300).start();
-            mButton.animate().alpha(0).setDuration(300).start();
-            mTextView.animate().alpha(1).setDuration(400).start();
-
         }
     }
 
-    private void cancelNotifications(long millisDelay, final Integer... notificationID) {
+    /**
+     * Helper function which allows our notifications to auto-cancel after a specified period of
+     * time.
+     * @param millisDelay delay after which to cancel the indicated notification(s).
+     * @param notificationIDs which notification to cancel.
+     */
+    private void cancelNotifications(long millisDelay, final Integer... notificationIDs) {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 StatusBarNotification[] notifications = mNotifManager.getActiveNotifications();
                 for (StatusBarNotification activeNotif : notifications) {
-                    for (int notifID : notificationID) {
+                    for (int notifID : notificationIDs) {
                         if (activeNotif.getId() == notifID) {
                             mNotifManager.cancel(notifID);
                         }
